3df321880557780286fd47077f1ed038f1d12543
pbarban <pierre.barban@gmail.com>
2021-11-01 22:10:15 +0100

    people and functions

--- INSEE.R
+++ INSEE.R
@@ -17,5 +17,37 @@ All_data = Pop.proj %>%
   group_by(sexe, age) %>%
   mutate(deaths = na_kalman(deaths, model = "auto.arima")) %>%  # Estimate nbr of deaths in 2050
   mutate(Mortality.rate = deaths/pop)
-  
 
+df = All_data %>% 
+  ungroup() %>% 
+  mutate(age_grp = age_grp(age),
+         Mortality.rate = ifelse(Mortality.rate > 1, 1, Mortality.rate)) # Some MR are >1, not excatly sure why...
+
+
+#Life expectancy table 
+library(ggplot2)
+life_exp(df, year,sexe,  Mortality.rate) 
+
+%>% 
+  ggplot() + geom_Line(aes(x = year, y = life_exp, color = sexe))
+
+
+
+life_exp_tab = data.frame("year" = 2020:2050, "life_exp" = rep(NA, length(2020:2050)))
+
+for (i in nrow(life_exp_tab)){
+  yy = life_exp_tab$year[i]
+  
+  tab = Pop.proj.both[Pop.proj.both$year == yy,]
+  tab = tab[order(tab$age),]
+  tab$Mortality.rate[tab$age == max(tab$age)] = 1
+  
+  prop_alive = c(1, cumprod((1 - tab$Mortality.rate) ))
+  deaths <- -diff(prop_alive)
+  life_exp = sum(deaths * 0:(max(tab$age)) ) 
+  life_exp_tab$life_exp[i] = life_exp
+}
+plot(life_exp_tab$year, life_exp_tab$life_exp)
+
+prop_alive
+deaths
--- functions.R
+++ functions.R
@@ -1,4 +1,4 @@
-
+# Test function
 test_fun = function(x) {
   if(is.numeric(x)){
     x = x +7
@@ -9,3 +9,55 @@ test_fun = function(x) {
   print(x)
 }
 
+
+# Download INSEE scenarios
+# Also cleaned the dataframe and give a long output instead of wide
+
+download_INSEE = function(scenario = "SP01"){
+  if (!require("pacman")) install.packages("pacman")
+  pacman::p_load(readxl,
+                 tidyr)
+  
+  temp <-  tempfile()
+  
+  if(substr(scenario,1,2) == "SP"){
+    id = 2530035 #ID for pop proj
+    value = "pop"
+  }
+  
+  else if(substr(scenario,1,2) == "SD") {
+    id = 2530048 #ID for mortality
+    value = "deaths"
+  }
+  
+  dataURL <- paste0("https://www.insee.fr/fr/statistiques/fichier/",id,"/projpop0550_",scenario,".xls")
+  download.file(dataURL, destfile=temp, mode='wb')
+  data <- readxl::read_excel(temp, skip = 4, col_names = TRUE)
+  
+  data_cleaned = data %>% 
+    na.omit() %>% 
+    rename( "age" = starts_with("AGE")) %>% 
+    mutate(age = ifelse(substr(age, nchar(age), nchar(age)) == "+",
+                        gsub('.{1}$', '', age),
+                        age),
+           sexe = ifelse(SEXE == "1", "Male", "Female")) %>% 
+    mutate(across(.cols = c(everything(), - sexe),  as.numeric)) %>% 
+    select(-SEXE) %>% 
+    pivot_longer(!c(age, sexe),
+                 names_to = "year",
+                 values_to = value ) 
+  
+  return(data_cleaned)
+}
+
+# function that create categories based on an age column
+
+age_grp = function(age){
+  age_grp = gsub(",", "-",
+                 gsub("\\[|\\]|\\(|\\)", "",
+                      cut( age, breaks = seq(0,150, by = 5), include.lowest = T, right = F)))
+  post = sub(".*-","",age_grp)
+  age_grp = paste0(sub("-.*", "", age_grp),
+                   "-", as.numeric(post)-1)
+  return(age_grp)
+}
