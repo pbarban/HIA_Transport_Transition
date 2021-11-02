55e16d3523a55c4d02c10788495feb77c96bc800
pbarban <pierre.barban@gmail.com>
2021-11-02 12:51:54 +0100

    fix the -inf problem

--- INSEE.R
+++ INSEE.R
@@ -16,56 +16,16 @@ All_data = Pop.proj %>%
   mutate(pop = ifelse(is.na(pop), 0, pop)) %>%
   group_by(sexe, age) %>%
   mutate(deaths = na_kalman(deaths, model = "auto.arima")) %>%  # Estimate nbr of deaths in 2050
-  mutate(Mortality.rate = deaths/pop)
+  mutate(MR = deaths/pop)
 
 df = All_data %>% 
   ungroup() %>% 
   mutate(age_grp = age_grp(age),
-         Mortality.rate = ifelse(Mortality.rate > 1, 1, Mortality.rate)) # Some MR are >1, not excatly sure why...
-
-life_exp = function(df, year,sexe, MR){
-test = df %>% 
-  group_by(year,sexe) %>% 
-  arrange(year,sexe) %>% 
-  mutate(prop_alive = cumprod(1 - MR),
-         deaths =  -(prop_alive - lag(prop_alive)),
-         life_exp = age *deaths) %>% 
-  group_by(sexe, year) %>% 
-  summarise(life_exp = sum(life_exp, na.rm = T)) 
-return(test)
-}
-
-
-life_exp(df, MR = Mortality.rate)
-
-
-
-
+         MR = ifelse(MR > 1 | pop == 0, 1, MR)) # Some MR are >1, not excatly sure why...
 
 #Life expectancy table 
 library(ggplot2)
-life_exp(df, year,sexe,  Mortality.rate) 
-
-test %>% 
-  ggplot() + geom_line(aes(x = year, y = life_exp, group = sexe, color = sexe))
-
-
-
-life_exp_tab = data.frame("year" = 2020:2050, "life_exp" = rep(NA, length(2020:2050)))
-
-for (i in nrow(life_exp_tab)){
-  yy = life_exp_tab$year[i]
-  
-  tab = Pop.proj.both[Pop.proj.both$year == yy,]
-  tab = tab[order(tab$age),]
-  tab$Mortality.rate[tab$age == max(tab$age)] = 1
-  
-  prop_alive = c(1, cumprod((1 - tab$Mortality.rate) ))
-  deaths <- -diff(prop_alive)
-  life_exp = sum(deaths * 0:(max(tab$age)) ) 
-  life_exp_tab$life_exp[i] = life_exp
-}
-plot(life_exp_tab$year, life_exp_tab$life_exp)
-
-prop_alive
-deaths
+life_exp(df, year,sexe,  df)  %>% 
+  ggplot() + 
+  geom_line(aes(x = year, y = life_exp, group = sexe, color = sexe)) +
+  theme(axis.text.x = element_text(angle = 90))
