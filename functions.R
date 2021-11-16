f4cea06706c9f0dfdbb6bb089163617c082d73ce
pbarban <pierre.barban@gmail.com>
2021-11-15 20:53:10 -0500

    add the "both" option in the download_INSEE function

--- functions.R
+++ functions.R
@@ -12,8 +12,11 @@ test_fun = function(x) {
 
 # Download INSEE scenarios
 # Also cleaned the dataframe and give a long output instead of wide
+# Option both always set as TRUE, add together women and men 
 
-download_INSEE = function(scenario = "SP01"){
+scenario = "SP01"
+
+download_INSEE = function(scenario = "SP01", both = T){
   if (!require("pacman")) install.packages("pacman")
   pacman::p_load(readxl,
                  tidyr)
@@ -22,18 +25,19 @@ download_INSEE = function(scenario = "SP01"){
   
   if(substr(scenario,1,2) == "SP"){
     id = 2530035 #ID for pop proj
-    value = "pop"
+    type = "pop"
   }
   
   else if(substr(scenario,1,2) == "SD") {
     id = 2530048 #ID for mortality
-    value = "deaths"
+    type = "deaths"
   }
   
   dataURL <- paste0("https://www.insee.fr/fr/statistiques/fichier/",id,"/projpop0550_",scenario,".xls")
   download.file(dataURL, destfile=temp, mode='wb')
   data <- readxl::read_excel(temp, skip = 4, col_names = TRUE)
   
+  if(both == T){
   data_cleaned = data %>% 
     na.omit() %>% 
     rename( "age" = starts_with("AGE")) %>% 
@@ -45,8 +49,30 @@ download_INSEE = function(scenario = "SP01"){
     select(-SEXE) %>% 
     pivot_longer(!c(age, sexe),
                  names_to = "year",
-                 values_to = value ) 
+                 values_to = all_of(type) ) %>% 
+    pivot_wider(names_from = sexe,
+                values_from = all_of(type)) %>% 
+    mutate(Both = Female + Male) %>% 
+    pivot_longer(!c(age, year),
+                 names_to = "sexe",
+                 values_to = all_of(type))
+  }
   
+  else{
+    data_cleaned = data %>% 
+      na.omit() %>% 
+      rename( "age" = starts_with("AGE")) %>% 
+      mutate(age = ifelse(substr(age, nchar(age), nchar(age)) == "+",
+                          gsub('.{1}$', '', age),
+                          age),
+             sexe = ifelse(SEXE == "1", "Male", "Female")) %>% 
+      mutate(across(.cols = c(everything(), - sexe),  as.numeric)) %>% 
+      select(-SEXE) %>% 
+      pivot_longer(!c(age, sexe),
+                   names_to = "year",
+                   values_to = all_of(type) )
+    
+  }
   return(data_cleaned)
 }
 
@@ -63,7 +89,7 @@ age_grp = function(age){
 }
 
 # Get life expectancy by year and sex
-life_exp = function(df, year,sexe, MR){
+life_exp = function(df, year,sexe, MR, ){
   test = df %>% 
     group_by(year,sexe) %>% 
     arrange(year,sexe) %>% 
