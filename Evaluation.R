d4e30a7c13363dee60eeb6dd64a0b4582811e5ef
kjean <kevin.jean.lab@gmail.com>
2021-11-19 16:52:16 +0100

    add lines corresponding to 0:14 and 85:100 in Denmark data Pierre, n'hésite pas à déplacer et traduire en dplyr si tu souhaites

--- Evaluation.R
+++ Evaluation.R
@@ -8,4 +8,19 @@ source("INSEE.R")
 source("negaWatt_data.R")
 
 
+#### First add lines corresponding to 0:14 and 85:100 in Denmark data
+dim(Denmark_data)
+den = Denmark_data %>% filter(sexe == "Both") ; dim(den)
+colnames(den)
 
+vec_age = rep(c(0:14, 85:100), 2)
+tmp = cbind(vec_age, rep(0, length(vec_age)),  rep(0, length(vec_age)), 
+            c(rep("cycling", length(vec_age)/2), rep("walking", length(vec_age)/2)), 
+              rep(0, length(vec_age)), rep(0, length(vec_age)) ) 
+colnames(tmp) = colnames(den)
+den  = rbind(den , tmp)
+rm(tmp)
+dim(den)
+den$age = as.numeric(den$age)
+den = den %>% arrange(mode, age)
+#Pierre, n'hésite pas à déplacer et traduire en dplyr si tu souhaites
