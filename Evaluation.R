7b82ab5f6720a79c9cade8437591fdd8a7ba4c40
kjean <kevin.jean.lab@gmail.com>
2021-11-22 11:09:23 +0100

    set parameters and convert mileage into time

--- Evaluation.R
+++ Evaluation.R
@@ -26,8 +26,35 @@ den = den %>% arrange(mode, age)
 #Pierre, n'hésite pas à déplacer et traduire en dplyr si tu souhaites
 
 
+
+
+#############################################################################################
+###############################
+# set parameters
+###############################
+#############################################################################################
+walk_Ref_volume <- 168
+walk_speed <- 4.8
+RR_walk = 0.89
+
+cycle_Ref_volume <- 100
+cycle_speed <- 14
+RR_cycle = 0.90
+
+# pour l'instant, on utilise les memes parametres VAE/vélo pour retrouver les résultats précédents
+vae_speed <-cycle_speed  # valeur de Bouscasse et al : 18
+MET_ratio <- 1# valeur de Bouscasse et al : 4.5/5.8
+
+#############################################################################################
+#############################################################################################
+#############################################################################################
+
 #### merge nw_data and INSEE_data
 INSEE_data$year = as.numeric(INSEE_data$year)
 nw = select(INSEE_data, year, p_tot) %>% 
   right_join(nw_data, by = "year") %>% 
-  mutate(total_km = p_tot*value)
+  rename(km_pp_y = value) %>% 
+  mutate(total_km_y = p_tot*km_pp_y,
+         speed = rep(c(walk_speed, cycle_speed, vae_speed), length.out = nrow(nw)),
+         min_pp_w = (60*km_pp_y /speed) / (365.25/7))  # weekly minutes
+
