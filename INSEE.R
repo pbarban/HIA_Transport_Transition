db4d010dcca9c5b53e94d6a96bd8bde5b35c72d4
pbarban <pierre.barban@gmail.com>
2021-11-23 08:43:13 -0500

    Switch life_expct to a function to measure it for all genders

--- INSEE.R
+++ INSEE.R
@@ -26,44 +26,45 @@ INSEE_data =  All_data %>%
   group_by(sexe, year) %>%
   mutate(p_tot =sum(pop),
         p_prop = pop/p_tot) %>% 
-  filter(sexe == "Both") %>% 
-  filter(year>2019)  %>% 
-  ungroup()
+  filter(year>2021)  %>% 
+  life_exp(MR,age) %>% 
+  mutate(yll = life_exp - age)
 
-# INSEE_data %>% 
-#   group_by(sexe, year) %>% 
-#   summarise(MR = sum(deaths)) %>% 
-#   ggplot() + 
-#   geom_line(aes(x = year,
-#                 y = MR,
-#                 group = sexe,
-#                 color = sexe ))+
-#   theme(axis.text.x = element_text(angle = 90))
+ INSEE_data %>% 
+   group_by(sexe, year) %>% 
+   summarise(MR = mean(life_exp)) %>% 
+   ggplot() + 
+   geom_line(aes(x = year,
+                 y = MR,
+                 group = sexe,
+                 color = sexe ))+
+   theme(axis.text.x = element_text(angle = 90))
   
 
 # calculate life expectancy
-life_exp_tab = data.frame("year" = 2020:2050, "life_exp" = rep(NA, length(2020:2050)))
+#life_exp_tab = data.frame("year" = 2020:2050, "life_exp" = rep(NA, length(2020:2050)))
 
-for (i in 1:nrow(life_exp_tab)){
-  yy = life_exp_tab$year[i]
+#for (i in 1:nrow(life_exp_tab)){
+#  yy = life_exp_tab$year[i]
   
-  tab = INSEE_data[INSEE_data$year == yy,]
-  tab = tab[order(tab$age),]
-  tab$MR[tab$age == max(tab$age)] = 1
+ # tab = INSEE_data[INSEE_data$year == yy,]
+ # tab = tab[order(tab$age),]
+ # tab$MR[tab$age == max(tab$age)] = 1
   
-  prop_alive = c(1, cumprod((1 - tab$MR) ))
-  deaths <- -diff(prop_alive)
-  life_exp = sum(deaths * 0:(max(tab$age)) ) 
-  life_exp_tab$life_exp[i] = life_exp
-}
-plot(life_exp_tab$year, life_exp_tab$life_exp)
+ # prop_alive = c(1, cumprod((1 - tab$MR) ))
+ # deaths <- -diff(prop_alive)
+ # life_exp = sum(deaths * 0:(max(tab$age)) ) 
+ # life_exp_tab$life_exp[i] = life_exp
+#}
+#plot(life_exp_tab$year, life_exp_tab$life_exp)
+
 
 #ajouter experence de vie esperance de vie - age = yll
-dim(INSEE_data)
-m_year = match(INSEE_data$year, life_exp_tab$year); length(m_year)
-INSEE_data = INSEE_data %>% 
-  mutate(life_exp = life_exp_tab$life_exp[m_year],
-         yll = life_exp-age)
+#dim(INSEE_data)
+#m_year = match(INSEE_data$year, life_exp_tab$year); length(m_year)
+#INSEE_data = INSEE_data %>% 
+ # mutate(life_exp = life_exp_tab$life_exp[m_year],
+ #        yll = life_exp-age)
 
 
 
